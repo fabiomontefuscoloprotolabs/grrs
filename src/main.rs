baf6df0fe743aa465978e65feb401dcb2bd6326a
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-04-02 06:50:39 +0200

    adding error context

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let content = std::fs::read_to_string("test.txt")?;
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), CustomError> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
     println!("file content: {}", content);
     Ok(())
 }
